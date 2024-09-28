7868607cc90b880a6059ffce4a0f081baa392768
Tylko-jedno <diogkek@gmail.com>
2024-09-28 18:59:07 +0300

    first master commit

---
+++ Lab05_1/Lab05_1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double k(const double x, const double y);
+
+int main()
+{
+	double p, q;
+
+	cout << "p = "; cin >> p;
+	cout << "q = "; cin >> q;
+
+	double c = k(p + sqrt(q), q - sqrt(p))*k(p + sqrt(q), q - sqrt(p)) - k(1, p + q);
+
+	cout << "c = " << c << endl;
+}
+
+double k(const double x, const double y)
+{
+	return x/abs(x*x*x + y*y*y) + y/abs(x + y);
+}
